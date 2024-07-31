bd5ecfc1b44ebd8493e5ada5f4432d3eef13f0cc
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-07-31 23:11:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Rkn_Bots(object):
     # Rkn client config
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = os.environ.get("API_ID", "22107616")
+    API_HASH = os.environ.get("API_HASH", "6629084d27f421f2375a57f233e471e2")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     #start_pic
@@ -35,7 +35,7 @@ class Rkn_Bots(object):
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
     #admin id
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('7417794229', '').split()]
     
 
 # Rkn Developer 
