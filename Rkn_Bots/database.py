68e9d7fb2a3f5c4a1c3b6030e5fef55521bac94e
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-20 18:27:06 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -1,45 +1,17 @@
-# (c) @RknDeveloperr
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+# database.py
 
-import motor.motor_asyncio
-from config import Rkn_Bots
+from pymongo import MongoClient
+from config import DB_URL, DB_NAME
 
-client = motor.motor_asyncio.AsyncIOMotorClient(Rkn_Bots.DB_URL)
-db = client[Rkn_Bots.DB_NAME]
-chnl_ids = db.chnl_ids
-users = db.users
+client = MongoClient(DB_URL)
+db = client[DB_NAME]
+users_coll = db["users"]
 
-#insert user data
-async def insert(user_id):
-    user_det = {"_id": user_id}
-    try:
-        await users.insert_one(user_det)
-    except:
-        pass
-        
-# Total User
-async def total_user():
-    user = await users.count_documents({})
-    return user
+def add_user(user_id: int):
+    """अगर user नया है तो database में डालो"""
+    if users_coll.count_documents({"user_id": user_id}, limit=1) == 0:
+        users_coll.insert_one({"user_id": user_id})
 
-async def getid():
-    all_users = users.find({})
-    return all_users
-
-async def delete(id):
-    await users.delete_one(id)
-                     
-async def addCap(chnl_id, caption):
-    dets = {"chnl_id": chnl_id, "caption": caption}
-    await chnl_ids.insert_one(dets)
-
-async def updateCap(chnl_id, caption):
-    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
-
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+def get_all_user_ids():
+    """सभी user_ids की list लौटाओ"""
+    return [d["user_id"] for d in users_coll.find({}, {"user_id": 1})]
