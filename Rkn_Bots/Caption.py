580aef707f2af694712d0cbd6ef17fc21c49f3c3
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-20 15:56:44 +0530

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -63,7 +63,7 @@ async def start_cmd(bot, message):
     await message.reply_photo(photo=Rkn_Bots.RKN_PIC,
         caption=f"<b>Hey, {message.from_user.mention}\n\nI'm an auto-caption bot. I automatically edit captions for videos, audio files, and documents posted on channels.\n\nuse <code>/set_caption</code> to set caption\nUse<code>/delcaption</code> To delete caption and set caption to default.\n\nNote:All commands works on channels only</b>",
         reply_markup=types.InlineKeyboardMarkup([[
-            types.InlineKeyboardButton('Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/+nvmpM5XeI9I0NTk1'),
+            types.InlineKeyboardButton('Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/wolverine273'),
             types.InlineKeyboardButton('Sᴜᴩᴩᴏʀᴛ', url='https://t.me/WOLVERIN_P')
         ]]))
     
